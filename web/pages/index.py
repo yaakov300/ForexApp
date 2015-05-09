793fb2c89e83c46fed03b1be5de79a1b8ea6f203
yaakov300 <yaacov300@gmail.com>
2015-05-10 00:05:56 +0300

    add index to pages for empty url

--- web/pages/index.py
+++ web/pages/index.py
@@ -3,13 +3,13 @@ __author__ = 'Oshri&Yaacov'
 from google.appengine.ext.webapp import template
 import webapp2
 
-class HomeHandler(webapp2.RequestHandler):
+class IndexHandler(webapp2.RequestHandler):
     def get(self):
         template_params = {}
         html = template.render("web/templates/home.html", template_params)
         self.response.write(html)
 
 app = webapp2.WSGIApplication([
-    ('/home', HomeHandler)
+    ('/', IndexHandler)
 ], debug=True)
 
