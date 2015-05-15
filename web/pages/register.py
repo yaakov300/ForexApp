bafd5681bbd06a5578eef9cd1e45f2b02b01d6eb
oshrim <oshrimaayan@gmail.com>
2015-05-15 11:02:50 +0300

    added some changes to register page

--- web/pages/register.py
+++ web/pages/register.py
@@ -1,6 +1,9 @@
 __author__ = 'Oshri'
 
 from google.appengine.ext.webapp import template
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
 import webapp2
 
 class RegisterHandler(webapp2.RequestHandler):
@@ -8,6 +11,17 @@ class RegisterHandler(webapp2.RequestHandler):
         template_params = {}
         html = template.render("web/templates/register.html", template_params)
         self.response.write(html)
+
+    def post(self):
+        userName = self.request.get("userName")
+        password = self.request.get("password")
+        confPass = self.request.get("confPass")
+        mail = self.request.get("mail")
+
+       
+
+
+        self.response.out.write(userName+password+confPass+mail)
 
 app = webapp2.WSGIApplication([
     ('/register', RegisterHandler)
