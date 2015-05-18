1a3af5345753b4da7773dcc9f70d6768212e431a
aviram120 <aviram1202@gmail.com>
2015-05-18 13:37:53 +0300

    add commend

--- web/pages/priceInterface.py
+++ web/pages/priceInterface.py
@@ -11,7 +11,6 @@ import requests  #Used for http requests
 def getCurrencyResults(FROM,TO):
     URL="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22"+ FROM +TO+\
         "%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
-
     request = requests.get(URL)
     data = request.json()#["query"]["results"]["rate"]["Rate"]
     return data, request.status_code
@@ -20,6 +19,7 @@ def getCommoditiesResults(COM):
     URL="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22"+COM+"%22)" \
         "&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
     request = requests.get(URL)
-    data = request.json()
+    data = request.json()#["query"]["results"]["quote"]["Ask"]
     return data, request.status_code
+
 
