e577fa16387975b82a322a627f376e0b212f3a8c
Oshri Maayan <oshrimaayan@gmail.com>
2015-05-20 11:10:44 +0300

    new table for alert

---
+++ models/alertDB.py
@@ -0,0 +1,12 @@
+
+
+from google.appengine.ext import ndb
+
+
+class Alert(ndb.Model):
+    symbol = ndb.StringProperty()
+    enterprice = ndb.StringProperty()
+    stoplose = ndb.StringProperty()
+    takeprofit = ndb.StringProperty()
+    volume = ndb.StringProperty()
+    date = ndb.DateTimeProperty()
