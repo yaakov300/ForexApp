9c2f76c6d3622274e673070c552107ff051633bc
Idan <idanbeck88@gmail.com>
2015-05-25 19:35:12 +0300

    change in login and base

--- web/pages/login.py
+++ web/pages/login.py
@@ -1,19 +1,30 @@
 __author__ = 'idan'
 from google.appengine.ext.webapp import template
 import webapp2
+import json
+from models.user import User
 
-class HomeHandler(webapp2.RequestHandler):
+
+class LoginHandler(webapp2.RequestHandler):
     def get(self):
+
+        username = self.request.get('userName')
+        password = self.request.get('password')
+
+        user = User.query(User.username == username).get()
+        if not user or not user.check_password(password):
+            self.error(402)
+            self.response.write('Wrong username or password')
+            return
+
+        self.response.set_cookie('our_token', str(user.key.id()))
+        self.response.write(json.dumps({'status':'OK'}))
+
         template_params = {}
         html = template.render("web/templates/login.html", template_params)
         self.response.write(html)
 
-
-
-
-
-
 app = webapp2.WSGIApplication([
-    ('/login', HomeHandler)
+    ('/login', LoginHandler)
 ], debug=True)
 
