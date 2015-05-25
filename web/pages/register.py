4059b5f111ed91b0c09c290b9191ed37ceb4ff1d
Idan <idanbeck88@gmail.com>
2015-05-25 18:07:55 +0300

    more change

--- web/pages/register.py
+++ web/pages/register.py
@@ -1,24 +1,46 @@
 from google.appengine.ext.webapp import template
 import webapp2
-
+import json
 from models.user import User
+
 
 class RegisterHandler(webapp2.RequestHandler):
     def get(self):
+
         template_params = {}
+
+        email = self.request.get('mail')
+        password = self.request.get('password')
+        user_name = self.request.get('userName')
+        first_name = self.request.get('firstName')
+        last_name = self.request.get('lastName')
+
+        user = User.query(User.username == user_name).get()
+
+        if user:
+                self.error(402)
+                self.response.write('user name is already in system')
+                return
+        user = User.query(User.mail == email).get()
+
+        if user:
+                self.error(402)
+                self.response.write('Email is already in system')
+                return
+
+        user = User()
+        user.mail = email
+        user.username = user_name
+        user.set_password(password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.put()
+
         html = template.render("web/templates/register.html", template_params)
         self.response.write(html)
 
-    def post(self):
-        uName = self.request.get('userName')
-        pwd = self.request.get('password')
-        ml = self.request.get('mail')
-        self.user = User(userName=uName ,mail=ml,password=pwd)
-        self.user.put()
-        self.response.write(uName+pwd+ml)
-
-
-
+        self.response.set_cookie('our_token', str(user.key.id()))
+        self.response.write(json.dumps({'status':'OK'}))
 
 app = webapp2.WSGIApplication([
     ('/register', RegisterHandler)
