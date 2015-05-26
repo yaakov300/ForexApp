ee837d5b9623b070874aaa36c042e417a97c4f3d
Idan <idanbeck88@gmail.com>
2015-05-26 18:23:56 +0300

    login change
    work but  not show user is login

--- web/pages/login.py
+++ web/pages/login.py
@@ -7,22 +7,34 @@ from models.user import User
 
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
-
-        username = self.request.get('userName')
-        password = self.request.get('password')
-
-        user = User.query(User.username == username).get()
-        if not user or not user.check_password(password):
-            self.error(402)
-            self.response.write('Wrong username or password')
-            return
-
-        self.response.set_cookie('our_token', str(user.key.id()))
-        self.response.write(json.dumps({'status':'OK'}))
-
         template_params = {}
         html = template.render("web/templates/login.html", template_params)
         self.response.write(html)
+
+
+    def post(self):
+       flag = True
+
+       while True:
+
+          username = self.request.get('userName')
+          password = self.request.get('pwd')
+
+          user = User.query(User.username == username).get()
+
+          if not user or not user.check_password(password):
+             self.error(403)
+             self.response.write('Wrong username or password')
+             flag = False
+
+          if flag == True:
+                break
+
+
+       self.response.set_cookie('our_token', str(user.key.id()))
+       self.response.write(json.dumps({'status':'OK'}))
+       self.redirect("/home")
+
 
 app = webapp2.WSGIApplication([
     ('/login', LoginHandler)
