3745ee482ce58b8e69af4253b9c6c909f146c43b
yaakov300 <yaacov300@gmail.com>
2015-06-04 11:27:29 +0300

    update app and cron yamal
    for cron job every day at 12:00 send mail

--- web/pages/mai.py
+++ web/pages/mai.py
@@ -1,35 +1,17 @@
-from google.appengine.ext import webapp
-import smtplib
-
+from google.appengine.api import mail
 import webapp2
 
-class ParseXMLHandler(webapp2.RequestHandler):
+
+class cronSendMail(webapp2.RequestHandler):
     def get(self):
+        user_address = "jceforrexapp@gmail.com"
+        senderadress = "aviram1202@gmail.com"
+        name = "yaacov the king"
+        comment = "temani is work"
+        mail.send_mail(senderadress, senderadress, "name: " + name + " mail: " + user_address , comment)
 
-        from_addr    = 'jceforexapp@gmail.com'
-        to_addr_list = ['yaacov300@gmail.com']
-        cc_addr_list = ['yaacov300@gmail.com']
-        subject      = 'Howdy'
-        message      = 'Howdy from a python function aviram is slave!!!!'
-        login        = 'jceforexapp'
-        password     = 'YOUforex'
-        sendemail(from_addr,to_addr_list,cc_addr_list,subject,message,login,password)
-
-def sendemail(from_addr, to_addr_list,cc_addr_list,subject, message,login, password,smtpserver='smtp.gmail.com:587'):
-        header  = 'From: %s\n' % from_addr
-        header += 'To: %s\n' % ','.join(to_addr_list)
-        header += 'Cc: %s\n' % ','.join(cc_addr_list)
-        header += 'Subject: %s\n\n' % subject
-        message = header + message
-
-        server = smtplib.SMTP(smtpserver)
-        server.starttls()
-        server.login(login,password)
-        problems = server.sendmail(from_addr, to_addr_list, message)
-        server.quit()
-
-application = webapp.WSGIApplication([
-    ('/cron', ParseXMLHandler)
+app = webapp2.WSGIApplication([
+    ('/cron', cronSendMail)
 ],debug=True)
 
 
