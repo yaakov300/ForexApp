4a7f25933ced9b80a3748c154f7f726883b42b51
yaakov300 <yaacov300@gmail.com>
2015-06-10 16:38:08 +0300

    update symboldb and cron

--- models/symbolGraphDB.py
+++ models/symbolGraphDB.py
@@ -1,10 +1,24 @@
 from google.appengine.ext import ndb
 
-class symbolGraph(ndb.Model):
-    symbolPrice = ndb.StringProperty(repeated=True)
+class symbolGraphDB(ndb.Model):
+    SP = ndb.StringProperty()
+    NSD = ndb.StringProperty()
+    DAX = ndb.StringProperty()
+    GOLD = ndb.StringProperty()
+    COIL = ndb.StringProperty()
+    ERU = ndb.StringProperty()
+    JPY = ndb.StringProperty()
+    CAD = ndb.StringProperty()
+    GPB = ndb.StringProperty()
+    AUD = ndb.StringProperty()
+    NZE = ndb.StringProperty()
+    CHF = ndb.StringProperty()
+    ILS = ndb.StringProperty()
+
+    #symbolPrice = ndb.StringProperty(repeated=True)
 
     @staticmethod
     def getsSymbolGraph():
-        return symbolGraph.query()
+        return symbolGraphDB.query()
 
 
--- web/pages/symbolGraph.py
+++ web/pages/symbolGraph.py
@@ -1,74 +1,50 @@
 import threading
-from models.symbolGraphDB import symbolGraph
+from models.symbolGraphDB import symbolGraphDB
 from threading import Lock
 import json  #use for create json
-#import requests  #Used for http requests
+from lib import requests   #Used for http requests
 import webapp2
 import logging
 
+
+stComm="ESM15.CME,NQM15.CME,GDAXI,GCM15.CMX,CLZ15.NYM"
 mutex = Lock()
 class symbolGraph(webapp2.RequestHandler):
+
     def get(self):
-        logging.info('from the cron')
+        logging.info('from the get')
         self.threading_minuets()
-        #self.multiRequests()
+
 
     def threading_minuets(self):
-        for i in range(10):
-            threading.Timer(1*i, self.multiRequests).start()
+        for i in range(20):
+            t = threading.Timer(0.1*i, self.multiRequests)
+            t.start()
+            t.join()
+        logging.info('end threading_minuets')
 
 
     #this function get all price for commodities and currency from yahoo api.
     def multiRequests(self):
-        logging.info('from the thread')
-        with open('static/json/symbols.json') as data_file:
-            data = json.load(data_file)
-
-        stComm=""
-        stCurr=""
-        stDax=""
-        line=0;
-        for i in data["symbol"]:
-            if (line<5):
-                stComm=stComm+''+i["symbolName"]+','
-            else:
-                stCurr=stCurr+''+i["symbolName"]+','
-            line=line+1
-        stComm=stComm[:-1]
-        stCurr=stCurr[:-1]
-
+        logging.info('from the multiRequests')
         URLComm="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22"+stComm+"%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
-
-        URLCurr="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22"+stCurr+\
-            "%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
-
-        requestComm = self.requests.get(URLComm)
+        requestComm = requests.get(URLComm)
         dataComm = requestComm.json()#["query"]["results"]["quote"]["Ask"]
 
-        requestCurr = self.requests.get(URLCurr)
-        dataCurr = requestCurr.json()#["query"]["results"]["rate"]["Rate"]
 
-        arrPrice = []
+        sp = dataComm["query"]["results"]["quote"][0]["Ask"]
+        nsd = dataComm["query"]["results"]["quote"][1]["Ask"]
+        dax = dataComm["query"]["results"]["quote"][2]["LastTradePriceOnly"]
+        gold = dataComm["query"]["results"]["quote"][3]["Ask"]
+        coil = dataComm["query"]["results"]["quote"][4]["Ask"]
+
         mutex.acquire()
-        try:
-            #print all Commodities
-            for i in range(dataComm["query"]["count"]):
-                if (i!=2):
-                    #print (dataComm["query"]["results"]["quote"][i]["Ask"])
-                    arrPrice.append(dataComm["query"]["results"]["quote"][i]["Ask"])
-                else:#for Dax
-                    #print (dataComm["query"]["results"]["quote"][i]["LastTradePriceOnly"])
-                    arrPrice.append(dataComm["query"]["results"]["quote"][i]["LastTradePriceOnly"])
-            #print all Currency
-            for i in range(dataCurr["query"]["count"]):
-                #print (dataCurr["query"]["results"]["rate"][i]["Rate"])
-                arrPrice.append(dataCurr["query"]["results"]["rate"][i]["Rate"])
-            print("arr start")
-            symbolGraph.symbolPrice = arrPrice
-            symbolGraph.put()
-        finally:
-            mutex.release()
+        logging.info('from the mutex')
 
+        symbols = symbolGraphDB(SP = sp, NSD =nsd, DAX = dax, GOLD = gold, COIL = coil)
+        symbols.put()
+
+        mutex.release()
 
 
 app = webapp2.WSGIApplication([
