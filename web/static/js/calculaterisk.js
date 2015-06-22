5bea3c84ebc54edb8e33acb931ee2f111e0da429
aviram120 <aviram1202@gmail.com>
2015-06-22 16:21:53 +0300

    fix bug

--- web/static/js/calculaterisk.js
+++ web/static/js/calculaterisk.js
@@ -65,7 +65,6 @@ function getCurrentPrice()
 
 function getCurrency(symbolName)
 {
-    console.log(symbolName);
     var xmlhttp = new XMLHttpRequest();
     var url ="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22"+symbolName+
         "%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
